34e864b0466a09919b524b7fe2459bea59426d9c
Robson Vaamonde <robsonvaamonde@gmail.com>
2024-07-18 20:18:28 -0300

    Atualizacao

--- script/openssh.sh
+++ script/openssh.sh
@@ -8,8 +8,8 @@
 # Instagram: https://www.instagram.com/procedimentoem/?hl=pt-br
 # Github: https://github.com/vaamonde
 # Data de criação: 14/07/2024
-# Data de atualização: 14/07/2024
-# Versão: 0.01
+# Data de atualização: 18/07/2024
+# Versão: 0.02
 # Testado e homologado para a versão do Ubuntu Server 22.04.x LTS x64
 #
 # Variável do endereço IPv4 Remoto do Ubuntu Server
@@ -17,7 +17,9 @@ IPADDRESS="172.16.1.20 22"
 #
 # Laço de Loop para testar a conexão simultânea com o servidor OpenSSH
 # Nesse loop será feita 20 (vinte) tentativas de conexão nas porta do SSH
+# utilizando o comando nc e tentando se conectar no IP e Porta indicado
 for i in {1..20}
 do
+	echo -n "Tentativa de conexão $i: "
 	echo 'exit' | nc $IPADDRESS
 done
